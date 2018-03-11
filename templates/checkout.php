8e719e4779a0ac4ece2acf4559ce22fe4b4a9dad
o.fadeev <o.fadeev@live.ru>
2018-03-11 13:20:34 +0300

    hw6-7-8

---
+++ templates/checkout.php
@@ -0,0 +1,425 @@
+<div class="page-default page-checkout" id="app" data-app="page-checkout">
+  <div class="wrapper">
+    <div class="page-title">
+      <h1>New Arrivals</h1>
+      <div class="breadcrumb">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Men</a></li>
+          <li><a href="#">New Arrivals</a></li>
+        </ul>
+      </div>
+    </div>
+    <section class="checkout-steps js-checkout-accordion">
+      <div class="step-row">
+        <div class="step-title">
+          01. <span class="title">Shipping Adress</span>
+        </div>
+        <div class="step-desc">
+          <div class="cols-cnt">
+            <div class="cols">
+              <form action="">
+                <div class="form-cnt">
+                  <div class="form-wrapper">
+                    <div class="form-header">
+                      <div class="form-title">
+                        Check as a guest or register
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                    </div>
+                    <div class="input-fields">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" checked />
+                          <i></i>
+                          checkout as guest
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" />
+                          <i></i>
+                          register
+                        </label>
+                      </div>
+                    </div>
+                    <div class="form-footer">
+                      <div class="form-title">
+                        register and save time!
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                      <div class="form-footer-desc">
+                        <ul>
+                          <li>
+                            Fast and easy checkout
+                          </li>
+                          <li>
+                            Easy access to your order history and status
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="submit">
+                      <input type="submit" class="btn" name="apply" value="Continue" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="cols">
+              <form action="">
+                <div class="form-cnt">
+                  <div class="form-wrapper">
+                    <div class="form-header">
+                      <div class="form-title">
+                        Already registed?
+                      </div>
+                      <div class="form-subtitle">
+                        Please log in below
+                      </div>
+                    </div>
+                    <div class="input-fields">
+                      <div class="input">
+                        <label>EMAIL ADDRESS <span class="required">*</span></label>
+                        <input type="text" name="email" value="" placeholder="Email*" data-validate="email" />
+                      </div>
+                      <div class="input">
+                        <label>PASSWORD <span class="required">*</span></label>
+                        <input type="password" name="password" value="" placeholder="Password*" data-validate="text" />
+                      </div>
+                    </div>
+                    <div class="form-footer">
+                      <div class="form-footer-desc">
+                        <div class="required">
+                          * Required Fileds
+                        </div>
+                      </div>
+                    </div>
+                    <div class="submit">
+                      <input type="submit" class="btn js-form-submit" name="apply" value="Log In" />
+                      <a href="#">Forgot Password?</a>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="step-row">
+        <div class="step-title">
+          02. <span class="title">BILLING INFORMATION</span>
+        </div>
+        <div class="step-desc">
+          <div class="cols-cnt">
+            <div class="cols">
+              <form action="">
+                <div class="form-cnt">
+                  <div class="form-wrapper">
+                    <div class="form-header">
+                      <div class="form-title">
+                        Check as a guest or register
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                    </div>
+                    <div class="input-fields">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" checked />
+                          <i></i>
+                          checkout as guest
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" />
+                          <i></i>
+                          register
+                        </label>
+                      </div>
+                    </div>
+                    <div class="form-footer">
+                      <div class="form-title">
+                        register and save time!
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                      <div class="form-footer-desc">
+                        <ul>
+                          <li>
+                            Fast and easy checkout
+                          </li>
+                          <li>
+                            Easy access to your order history and status
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="submit">
+                      <input type="submit" class="btn" name="apply" value="Continue" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="step-row">
+        <div class="step-title">
+          03. <span class="title">SHIPPING INFORMATION</span>
+        </div>
+        <div class="step-desc">
+          <div class="cols-cnt">
+            <div class="cols">
+              <form action="">
+                <div class="form-cnt">
+                  <div class="form-wrapper">
+                    <div class="form-header">
+                      <div class="form-title">
+                        Check as a guest or register
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                    </div>
+                    <div class="input-fields">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" checked />
+                          <i></i>
+                          checkout as guest
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" />
+                          <i></i>
+                          register
+                        </label>
+                      </div>
+                    </div>
+                    <div class="form-footer">
+                      <div class="form-title">
+                        register and save time!
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                      <div class="form-footer-desc">
+                        <ul>
+                          <li>
+                            Fast and easy checkout
+                          </li>
+                          <li>
+                            Easy access to your order history and status
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="submit">
+                      <input type="submit" class="btn" name="apply" value="Continue" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="step-row">
+        <div class="step-title">
+          04. <span class="title">SHIPPING METHOD</span>
+        </div>
+        <div class="step-desc">
+          <div class="cols-cnt">
+            <div class="cols">
+              <form action="">
+                <div class="form-cnt">
+                  <div class="form-wrapper">
+                    <div class="form-header">
+                      <div class="form-title">
+                        Check as a guest or register
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                    </div>
+                    <div class="input-fields">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" checked />
+                          <i></i>
+                          checkout as guest
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" />
+                          <i></i>
+                          register
+                        </label>
+                      </div>
+                    </div>
+                    <div class="form-footer">
+                      <div class="form-title">
+                        register and save time!
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                      <div class="form-footer-desc">
+                        <ul>
+                          <li>
+                            Fast and easy checkout
+                          </li>
+                          <li>
+                            Easy access to your order history and status
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="submit">
+                      <input type="submit" class="btn" name="apply" value="Continue" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="step-row">
+        <div class="step-title">
+          05. <span class="title">PAYMENT METHOD</span>
+        </div>
+        <div class="step-desc">
+          <div class="cols-cnt">
+            <div class="cols">
+              <form action="">
+                <div class="form-cnt">
+                  <div class="form-wrapper">
+                    <div class="form-header">
+                      <div class="form-title">
+                        Check as a guest or register
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                    </div>
+                    <div class="input-fields">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" checked />
+                          <i></i>
+                          checkout as guest
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" />
+                          <i></i>
+                          register
+                        </label>
+                      </div>
+                    </div>
+                    <div class="form-footer">
+                      <div class="form-title">
+                        register and save time!
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                      <div class="form-footer-desc">
+                        <ul>
+                          <li>
+                            Fast and easy checkout
+                          </li>
+                          <li>
+                            Easy access to your order history and status
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="submit">
+                      <input type="submit" class="btn" name="apply" value="Continue" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="step-row">
+        <div class="step-title">
+          06. <span class="title">ORDER REVIEW</span>
+        </div>
+        <div class="step-desc">
+          <div class="cols-cnt">
+            <div class="cols">
+              <form action="">
+                <div class="form-cnt">
+                  <div class="form-wrapper">
+                    <div class="form-header">
+                      <div class="form-title">
+                        Check as a guest or register
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                    </div>
+                    <div class="input-fields">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" checked />
+                          <i></i>
+                          checkout as guest
+                        </label>
+                      </div>
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="1" />
+                          <i></i>
+                          register
+                        </label>
+                      </div>
+                    </div>
+                    <div class="form-footer">
+                      <div class="form-title">
+                        register and save time!
+                      </div>
+                      <div class="form-subtitle">
+                        Register with us for future convenience
+                      </div>
+                      <div class="form-footer-desc">
+                        <ul>
+                          <li>
+                            Fast and easy checkout
+                          </li>
+                          <li>
+                            Easy access to your order history and status
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="submit">
+                      <input type="submit" class="btn" name="apply" value="Continue" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
