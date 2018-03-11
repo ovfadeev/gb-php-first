831cb5e415d9bfa10f027bdffeb5968610eefce9
o.fadeev <o.fadeev@live.ru>
2018-03-11 18:01:51 +0300

    hw6-7-8

---
+++ templates/contacts.php
@@ -0,0 +1,11 @@
+<div class="page-default" id="app" data-app="page-default">
+  <div class="wrapper">
+    <div class="page-title">
+      <h1><?=$content["title"]?></h1>
+      <?include("other/breadcrumbs.php");?>
+    </div>
+    <section>
+
+    </section>
+  </div>
+</div>
