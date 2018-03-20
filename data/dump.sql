d38bd5d9abfea5a982c74e5989bc4da6c4369bbf
ofadeev <ofadeev@maxlevel.ru>
2018-03-20 13:49:36 +0300

    hw 6-7-8

--- data/dump.sql
+++ data/dump.sql
@@ -13,15 +13,14 @@
 
 
 -- Дамп структуры базы данных gb-php
-DROP DATABASE IF EXISTS `gb-php`;
 CREATE DATABASE IF NOT EXISTS `gb-php` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `gb-php`;
 
 -- Дамп структуры для таблица gb-php.s_basket
-DROP TABLE IF EXISTS `s_basket`;
 CREATE TABLE IF NOT EXISTS `s_basket` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `user_id` int(255) DEFAULT NULL,
+  `session_id` int(255) NOT NULL DEFAULT '0',
   `products` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -32,7 +31,6 @@ DELETE FROM `s_basket`;
 /*!40000 ALTER TABLE `s_basket` ENABLE KEYS */;
 
 -- Дамп структуры для таблица gb-php.s_catalog_category
-DROP TABLE IF EXISTS `s_catalog_category`;
 CREATE TABLE IF NOT EXISTS `s_catalog_category` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `parent_id` int(255) DEFAULT NULL,
@@ -58,7 +56,6 @@ INSERT INTO `s_catalog_category` (`id`, `parent_id`, `name`, `code`) VALUES
 /*!40000 ALTER TABLE `s_catalog_category` ENABLE KEYS */;
 
 -- Дамп структуры для таблица gb-php.s_catalog_products
-DROP TABLE IF EXISTS `s_catalog_products`;
 CREATE TABLE IF NOT EXISTS `s_catalog_products` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `category_id` int(255) DEFAULT NULL,
@@ -68,6 +65,8 @@ CREATE TABLE IF NOT EXISTS `s_catalog_products` (
   `image_slider` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `price` decimal(10,0) DEFAULT NULL,
+  `size_id` int(11) DEFAULT NULL,
+  `color_id` int(255) DEFAULT '0',
   `status` int(255) DEFAULT NULL,
   `view` int(255) DEFAULT NULL,
   `count` int(255) DEFAULT NULL,
@@ -77,39 +76,62 @@ CREATE TABLE IF NOT EXISTS `s_catalog_products` (
 -- Дамп данных таблицы gb-php.s_catalog_products: ~15 rows (приблизительно)
 DELETE FROM `s_catalog_products`;
 /*!40000 ALTER TABLE `s_catalog_products` DISABLE KEYS */;
-INSERT INTO `s_catalog_products` (`id`, `category_id`, `name`, `image`, `image_small`, `image_slider`, `description`, `price`, `status`, `view`, `count`) VALUES
-	(1, 5, 'Футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 200, 1, 0, 10),
-	(2, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 1, 0, 10),
-	(3, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 1, 0, 10),
-	(4, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 1, 0, 10),
-	(5, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 1, 0, 10),
-	(6, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 2, 0, 10),
-	(7, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 2, 0, 10),
-	(8, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 2, 0, 10),
-	(9, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 2, 0, 10),
-	(10, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 2, 0, 10),
-	(11, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 0, 0, 10),
-	(12, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 0, 0, 10),
-	(13, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 0, 0, 10),
-	(14, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 0, 0, 10),
-	(15, 5, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 0, 0, 10);
+INSERT INTO `s_catalog_products` (`id`, `category_id`, `name`, `image`, `image_small`, `image_slider`, `description`, `price`, `size_id`, `color_id`, `status`, `view`, `count`) VALUES
+	(1, 1, 'Футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 200, 5, 1, 1, 0, 10),
+	(2, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 5, 2, 1, 0, 10),
+	(3, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 5, 2, 1, 0, 10),
+	(4, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 5, 2, 1, 0, 10),
+	(5, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 5, 2, 1, 0, 10),
+	(6, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 4, 2, 2, 0, 10),
+	(7, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 1, 2, 2, 0, 10),
+	(8, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 2, 2, 2, 0, 10),
+	(9, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 3, 2, 2, 0, 10),
+	(10, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 6, 2, 2, 0, 10),
+	(11, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 7, 2, 0, 0, 10),
+	(12, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 3, 2, 0, 0, 10),
+	(13, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 4, 2, 0, 0, 10),
+	(14, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 4, 2, 0, 0, 10),
+	(15, 1, 'футболка', '/images/product1.jpg', '/images/product-cart-small.jpg', '/images/productslide1.jpg', 'Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals.', 250, 4, 2, 0, 0, 10);
 /*!40000 ALTER TABLE `s_catalog_products` ENABLE KEYS */;
 
--- Дамп структуры для таблица gb-php.s_catalog_size
-DROP TABLE IF EXISTS `s_catalog_size`;
-CREATE TABLE IF NOT EXISTS `s_catalog_size` (
+-- Дамп структуры для таблица gb-php.s_catalog_product_color
+CREATE TABLE IF NOT EXISTS `s_catalog_product_color` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `value_hex` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `value_rgb` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Дамп данных таблицы gb-php.s_catalog_product_color: ~2 rows (приблизительно)
+DELETE FROM `s_catalog_product_color`;
+/*!40000 ALTER TABLE `s_catalog_product_color` DISABLE KEYS */;
+INSERT INTO `s_catalog_product_color` (`id`, `name`, `value_hex`, `value_rgb`) VALUES
+	(1, 'Black', '#000000', 'rgba(0, 0, 0, 1);'),
+	(2, 'White', '#FFFFFF', 'rgba(255, 255, 255, 1);');
+/*!40000 ALTER TABLE `s_catalog_product_color` ENABLE KEYS */;
+
+-- Дамп структуры для таблица gb-php.s_catalog_product_size
+CREATE TABLE IF NOT EXISTS `s_catalog_product_size` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `value` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Дамп данных таблицы gb-php.s_catalog_size: ~0 rows (приблизительно)
-DELETE FROM `s_catalog_size`;
-/*!40000 ALTER TABLE `s_catalog_size` DISABLE KEYS */;
-/*!40000 ALTER TABLE `s_catalog_size` ENABLE KEYS */;
+-- Дамп данных таблицы gb-php.s_catalog_product_size: ~7 rows (приблизительно)
+DELETE FROM `s_catalog_product_size`;
+/*!40000 ALTER TABLE `s_catalog_product_size` DISABLE KEYS */;
+INSERT INTO `s_catalog_product_size` (`id`, `value`) VALUES
+	(1, 'XS'),
+	(2, 'S'),
+	(3, 'M'),
+	(4, 'L'),
+	(5, 'XL'),
+	(6, 'XXL'),
+	(7, 'XXXL');
+/*!40000 ALTER TABLE `s_catalog_product_size` ENABLE KEYS */;
 
 -- Дамп структуры для таблица gb-php.s_reviews
-DROP TABLE IF EXISTS `s_reviews`;
 CREATE TABLE IF NOT EXISTS `s_reviews` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `user_id` int(255) NOT NULL,
@@ -129,7 +151,6 @@ INSERT INTO `s_reviews` (`id`, `user_id`, `product_id`, `name`, `text`) VALUES
 /*!40000 ALTER TABLE `s_reviews` ENABLE KEYS */;
 
 -- Дамп структуры для таблица gb-php.s_subscribe
-DROP TABLE IF EXISTS `s_subscribe`;
 CREATE TABLE IF NOT EXISTS `s_subscribe` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -145,7 +166,6 @@ INSERT INTO `s_subscribe` (`id`, `email`) VALUES
 /*!40000 ALTER TABLE `s_subscribe` ENABLE KEYS */;
 
 -- Дамп структуры для таблица gb-php.s_users
-DROP TABLE IF EXISTS `s_users`;
 CREATE TABLE IF NOT EXISTS `s_users` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -160,7 +180,7 @@ CREATE TABLE IF NOT EXISTS `s_users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Дамп данных таблицы gb-php.s_users: ~2 rows (приблизительно)
+-- Дамп данных таблицы gb-php.s_users: ~1 rows (приблизительно)
 DELETE FROM `s_users`;
 /*!40000 ALTER TABLE `s_users` DISABLE KEYS */;
 INSERT INTO `s_users` (`id`, `login`, `password`, `email`, `f_name`, `l_name`, `image`, `address`, `date_register`, `last_date_auth`) VALUES
